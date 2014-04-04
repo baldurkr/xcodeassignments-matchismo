72296f5088a4b98103a324396449cbda45dbb874
Baldur Kristjánsson <baldur@Baldur-MacBook.local>
2014-04-04 12:56:28 +0000

    Kláraði IB hluta

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -9,10 +9,40 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flips;
 
 @end
 
 @implementation CardGameViewController
+
+@synthesize flips = _flips;
+
+-(void)setFlips:(int) count
+{
+    _flips = count;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", count];
+    NSLog(@"flipCount changed to %d", count);
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender
+{
+    if([sender.currentTitle length])
+    {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flips++;
+    }
+    else
+    {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                forState:UIControlStateNormal];
+        [sender setTitle:@"A♣️" forState:UIControlStateNormal];
+        self.flips++;
+    }
+    
+}
 
 - (void)viewDidLoad
 {
